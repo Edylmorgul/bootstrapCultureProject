623a537eb8f2bd71ee6ee74b600ab11635b25215
Edylmorgul <loicpierard@outlook.com>
2021-06-06 11:17:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".navbar").hide();
+
+    $(function(){
+        $(window).scroll(function(){
+            if($(this).scrollTop() > 140){
+                $(".navbar").fadeIn();
+            }
+            else{
+                $(".navbar").fadeOut();
+            }
+        });
+    });
+});
